b2fd6f1a82be0014d65b6615ee3f281aa0aa839f
Tony Grimes <acidtone@tonygrimes.com>
2020-11-17 19:22:34 -0700

    initial commit

---
+++ week11t/index.php
@@ -0,0 +1,30 @@
+<pre>
+
+<?php
+// echo "Hello World!";
+
+$people = array('Kevin', 'Jeremy', 'Sara');
+
+// echo $people[2];
+
+// echo $people;
+
+var_dump($people);
+
+// print_r($people);
+
+$num = 6;
+
+if($num > 4){
+  if($num < 10){
+    echo "$num passed";
+  } else {
+    echo 'whatever';
+  }
+}
+
+?>
+
+</pre>
+
+<h1><? echo true; ?></h1>
